e764d480118d0c376b3103e4d2327888af96b2b2
Souravji <linges.tsm@gmail.com>
2019-12-09 16:56:25 +0530

    Files

---
+++ .github/workflows/s1.js
@@ -0,0 +1,12 @@
+var subtract = function (a, b) {
+    return a-b;
+}
+
+var handle_data = function () {
+    // get data from user or other external source
+    var x = 2;
+    var y = 3;
+	return (x,y);
+    //return func(x, y);
+}
+console.log(handle_data(subtract)); 
